72aff5dbbc11aacc15a8bf05c5bd09e42f45da1a
Victor Tenneroni <vitenner@c1r1s4.42singapore.sg>
2024-08-22 18:34:01 +0800

    fixed music

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the library path
+export LD_LIBRARY_PATH=./audio_lib:$LD_LIBRARY_PATH
+
+# Run the program
+./cube3dwaudio
+
+# Optionally, you can add command line arguments like this:
+# ./cube3dwaudio "$@"
